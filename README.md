a13096ec02d5291c69a96d9bbe17d02fbec7c467
Michael Robinson <merobi@gmail.com>
2022-05-20 13:53:33 -0400

    add a README and support for arguments specifying an author and a file type
    Signed-off-by: Michael Robinson <merobi@gmail.com>

---
+++ README.md
@@ -0,0 +1,22 @@
+Supported file types:
+
+.java
+.py
+.md
+.html
+.txt
+.rs
+.sh
+.ts
+
+To use:
+
+1. Copy the add-copyright.py script and paste in the root directory of your project.
+
+2. Run the script with Python 3, using the project author's name as the first argument and the desired file type as the second argument.
+
+`python3 add-copyright.py Marquez sh`
+
+The above command will add a copyright attributing authorship to Marquez to all Bash files in the project.
+
+
--- add-copyright.py
+++ add-copyright.py
@@ -1,6 +1,11 @@
 def add_copyright():
 
     import os
+    import sys
+
+    project = str(sys.argv[1])
+    file_type = str(sys.argv[2])
+    print(f'adding copyright lines to all .{file_type} files...')
 
     # Create an array of paths mirroring structure of current directory
     # while ignoring this script and problematic file types
@@ -24,7 +29,7 @@ def add_copyright():
             else:
                 path = os.path.join(root, f)
                 paths.append(path)
-    print(paths)
+    print(f'{len(paths)} files found')
 
     # Iterate through file paths, adding a copyright line to each file
     for p in paths:
@@ -35,61 +40,80 @@ def add_copyright():
         elif '.gitignore' in p:
             continue
         else:        
-            if p[-4:] == 'java':
-                with open(p, 'r') as t:
-                    contents = t.readlines()
-                    contents.insert(0, '/* Copyright 2018-2022 contributors to the Marquez project */\n\n')
-                with open(p, 'w') as t:
-                    contents = ''.join(contents)
-                    t.write(contents)
-            elif p[-2:] == 'py':
-                with open(p, 'r') as t:
-                    contents = t.readlines()
-                    contents.insert(0, '# Copyright 2018-2022 contributors to the Marquez project\n\n')
-                with open(p, 'w') as t:
-                    contents = ''.join(contents)
-                    t.write(contents)
-            elif p[-2:] == 'md':
-                with open(p, 'r') as t:
-                    contents = t.readlines()
-                    contents.insert(0, '<!-- Copyright 2018-2022 contributors to the Marquez project -->\n\n')
-                with open(p, 'w') as t:
-                    contents = ''.join(contents)
-                    t.write(contents)
-            elif p[-2:] == 'html':
-                with open(p, 'r') as t:
-                    contents = t.readlines()
-                    contents.insert(0, '<!-- Copyright 2018-2022 contributors to the Marquez project -->\n\n')
-                with open(p, 'w') as t:
-                    contents = ''.join(contents)
-                    t.write(contents)
-            elif p[-3:] == 'txt':
-                with open(p, 'r') as t:
-                    contents = t.readlines()
-                    contents.insert(0, 'Copyright 2018-2022 contributors to the Marquez project\n\n')
-                with open(p, 'w') as t:
-                    contents = ''.join(contents)
-                    t.write(contents)
-            elif p[-2:] == 'rs':
-                with open(p, 'r') as t:
-                    contents = t.readlines()
-                    contents.insert(0, '// Copyright 2018-2022 contributors to the Marquez project\n\n')
-                with open(p, 'w') as t:
-                    contents = ''.join(contents)
-                    t.write(contents)
-            elif p[-2:] == 'sh':
-                with open(p, 'r') as t:
-                    contents = t.readlines()
-                    contents.insert(0, '# Copyright 2018-2022 contributors to the Marquez project\n\n')
-                with open(p, 'w') as t:
-                    contents = ''.join(contents)
-                    t.write(contents)
-            elif p[-2:] == 'ts':
-                with open(p, 'r') as t:
-                    contents = t.readlines()
-                    contents.insert(0, '// Copyright 2018-2022 contributors to the Marquez project\n\n')
-                with open(p, 'w') as t:
-                    contents = ''.join(contents)
-                    t.write(contents)
+            if file_type == 'java':
+                if p[-4:] == 'java':
+                    with open(p, 'r') as t:
+                        contents = t.readlines()
+                        line = f'/* Copyright 2018-2022 contributors to the {project} project */\n\n'
+                        contents.insert(0, line)
+                    with open(p, 'w') as t:
+                        contents = ''.join(contents)
+                        t.write(contents)
+            elif file_type == 'py':
+                if p[-2:] == 'py':
+                    with open(p, 'r') as t:
+                        contents = t.readlines()
+                        line = f'# Copyright 2018-2022 contributors to the {project} project\n\n'
+                        contents.insert(0, line)
+                    with open(p, 'w') as t:
+                        contents = ''.join(contents)
+                        t.write(contents)
+            elif file_type == 'md':
+                if p[-2:] == 'md':
+                    with open(p, 'r') as t:
+                        contents = t.readlines()
+                        line = f'<!-- Copyright 2018-2022 contributors to the {project} project -->\n\n'
+                        contents.insert(0, line)
+                    with open(p, 'w') as t:
+                        contents = ''.join(contents)
+                        t.write(contents)
+            elif file_type == 'html':
+                if p[-2:] == 'html':
+                    with open(p, 'r') as t:
+                        contents = t.readlines()
+                        line = f'<!-- Copyright 2018-2022 contributors to the {project} project -->\n\n'
+                        contents.insert(0, line)
+                    with open(p, 'w') as t:
+                        contents = ''.join(contents)
+                        t.write(contents)
+            elif file_type == 'txt':
+                if p[-3:] == 'txt':
+                    with open(p, 'r') as t:
+                        contents = t.readlines()
+                        line = f'Copyright 2018-2022 contributors to the {project} project\n\n'
+                        contents.insert(0, line)
+                    with open(p, 'w') as t:
+                        contents = ''.join(contents)
+                        t.write(contents)
+            elif file_type == 'rs':
+                if p[-2:] == 'rs':
+                    with open(p, 'r') as t:
+                        contents = t.readlines()
+                        line = f'// Copyright 2018-2022 contributors to the {project} project\n\n'
+                        contents.insert(0, line)
+                    with open(p, 'w') as t:
+                        contents = ''.join(contents)
+                        t.write(contents)
+            elif file_type == 'sh':
+                if p[-2:] == 'sh':
+                    with open(p, 'a') as t:
+                        line = f'\n# Copyright 2018-2022 contributors to the {project} project'
+                        t.write(line)
+                    #     contents = t.readlines()
+                    #     contents.insert(0, '# Copyright 2018-2022 contributors to the {project} project\n\n')
+                    # with open(p, 'w') as t:
+                    #     contents = ''.join(contents)
+                    #     t.write(contents)
+            elif file_type == 'ts':
+                if p[-2:] == 'ts':
+                    with open(p, 'r') as t:
+                        contents = t.readlines()
+                        line = f'// Copyright 2018-2022 contributors to the {project} project\n\n'
+                        contents.insert(0, line)
+                    with open(p, 'w') as t:
+                        contents = ''.join(contents)
+                        t.write(contents)
+    
+    print('Done!')
 
 add_copyright()
